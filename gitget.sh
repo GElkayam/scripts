6ba69acc395f04f40ad7e8e095b33d61262ddec7
GElkayam <Guy.Elkayam@Kaltura.com>
2014-12-26 03:31:28 +0200

    added switch parsing

--- gitget.sh
+++ gitget.sh
@@ -2,19 +2,47 @@
 # Guy Elkayam 25-Dec-2014. Merry XMas.
 # Download Binaries from GitHub Releases
 
-if [ $# -ne 3 ] ; then
-	echo "ERROR: Usage \"$0 UserName:PassWord Org repo\""
-	echo "ERROR: For debug run \"DEBUG=true $0 UserName:PassWord Org repo\""
+SUPPORTED="-up -r -o -l -d"
+DOWNLOAD=true
+
+while [ $# -gt 0 ]; do
+	[ $DEBUG ] && echo $1 $2
+	case $1 in
+		-up) USERPASS=$2
+		shift 2
+		;;
+		-o) ORG=$2
+		shift 2 ;;
+		-r) REPO=$2 
+		shift 2 ;;
+		-l) LATEST=true 
+		shift 1 ;;
+		-d) DEBUG=true 
+		shift 1 ;;
+		-dd) DOWNLOAD=false
+		shift 1 ;;
+		*) echo ERROR: $1 is unsupported. Only $SUPPORTED are supported.
+		exit 1
+		;;
+	esac
+done
+
+if [  ! -n "$USERPASS"  -o  ! -n "$ORG" -o ! -n "$REPO" ] ; then
+	echo "ERROR: Usage \"$0 -up UserName:PassWord -o Org -r repo\""
+	echo "ERROR: For debug run \"DEBUG=true $0 -up UserName:PassWord -o Org -r repo\""
+	[ $DEBUG ] && echo "ERROR: Usage \"$0 -up $USERPASS -o $ORG -r $REPO\""
+	[ -z "$USERPASS" ] && echo ERROR: Missing Username and Password. supply them as -up user:pass
+	[ -z "$ORG" ] && echo ERROR: Missing Org or Account. supply it as -o OrgName
+	[ -z "$USERPASS" ] && echo ERROR: Missing Repository name. supply it as -r RepositoryName
+
 	exit 1
 fi
-USERPASS=$1
-ORG=$2
-REPO=$3
+
 [ ! -e JSON.sh ] && echo Fetching JSON.sh from GitHub && curl -s https://raw.githubusercontent.com/dominictarr/JSON.sh/master/JSON.sh -o JSON.sh
 
-[ $DEBUG ] && echo "got $USERPASS and $ORG and $REPO"
+[ $DEBUG ] && echo "userpass: $USERPASS \n Org: $ORG \n Repo: $REPO \n Download: $DOWNLOAD"
 
-[ $DEBUG ] && echo "curl -s -u $USERPASS https://api.github.com/repos/$ORG/$REPO/releases | sh ./JSON.sh -b | grep "^[" |grep -v uploader | grep assets | grep "\"name\"\|\"url\"""
+[ $DEBUG ] && echo "curl -s -u $USERPASS https://api.github.com/repos/$ORG/$REPO/releases | sh ./JSON.sh -b | grep "^[0" |grep -v uploader | grep assets | grep "\"name\"\|\"url\"""
 IFS=$'\n' 
 [ $LATEST ] && COMMAND=`curl -s -u $USERPASS https://api.github.com/repos/$ORG/$REPO/releases | sh ./JSON.sh -b | grep "^\[0," |grep -v uploader | grep assets | grep "\"name\"\|\"url\""`
 [ ! $LATEST ] && COMMAND=`curl -s -u $USERPASS https://api.github.com/repos/$ORG/$REPO/releases | sh ./JSON.sh -b | grep -v uploader | grep assets | grep "\"name\"\|\"url\""`
@@ -26,7 +54,7 @@ do
 	if [[ $line == *name* ]] ; then 
 		NAME=$VALUE
 		echo Downloading $NAME as referenced in $URL
-		curl -s -u $USERPASS -L -H "Accept:application/octet-stream" ${URL//\"} -o ${NAME//\"}
-		[ -e ${NAME//\"} ] && echo ${NAME//\"} downloaded successfully. || echo ERROR downloading ${NAME//\"}.
+		$DOWNLOAD && curl -s -u $USERPASS -L -H "Accept:application/octet-stream" ${URL//\"} -o ${NAME//\"}
+		[ -e ${NAME//\"} ] && echo ${NAME//\"} downloaded successfully. || echo ERROR: ${NAME//\"} was not downloaded .
 	fi
 done
